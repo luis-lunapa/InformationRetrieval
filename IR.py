c636bbd1313a7efe819595105f171701a9eb1399
Victor Eduardo Pulido Contreras <victoredu96@MacBook-Pro-de-Victor-2.local>
2018-08-20 19:22:22 -0500

    Avances Significativos

--- IR.py
+++ IR.py
@@ -1,21 +1,33 @@
+documentsDictionary = { "texto.txt" :0,
+                        "texto1.txt" :1,
+                        "texto2.txt" :2
+                        }
+
 def leerArchivo():
-    salida = []
-    with open("documento1.txt", "r") as archivo:
-        lineas = [linea.split() for linea in archivo]
-
-    for linea in lineas:
-        salida += linea
-
-    #print(salida)
-
     dictionary = {}
-    numPalabras = (len(salida))
-    keys = range(numPalabras)
-    values = salida
 
-    for indice in keys:
-        dictionary[indice] = values[indice]
+    for documento in documentsDictionary:
+        palabrasSinToken = []
+        with open(documento, "r") as archivo:
+            lineas = [linea.split() for linea in archivo]
+
+            for linea in lineas:
+                palabrasSinToken += linea
+            #print(palabrasSinToken)
+
+            for palabra in palabrasSinToken:
+                if palabra in dictionary:
+                    if not (documentsDictionary[documento] in dictionary[palabra]):
+                        dictionary[palabra] += [documentsDictionary[documento]]
+                else:
+                    dictionary[palabra] = [documentsDictionary[documento]]
     print(dictionary)
+
+    aBuscar = input("Escribe la palabra a buscar: ")
+    encontrada = False
+
+    print(dictionary.get(aBuscar))
+
 
 if __name__ == "__main__":
     print('Leyendo Archivo...')
