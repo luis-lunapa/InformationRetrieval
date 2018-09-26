86102e25791552101afb66a06e63b5c157fd7088
luisLuna24 <luis.lunapa@outlook.com>
2018-09-25 22:03:26 -0500

    Proyecto 2

---
+++ Proyecto 2/Cranfield/Cranfield/StringFile.swift
@@ -0,0 +1,9 @@
+//
+//  StringFile.swift
+//  Cranfield
+//
+//  Created by Luis Luna on 9/14/18.
+//  Copyright © 2018 Luis Luna. All rights reserved.
+//
+
+import Foundation
---
+++ Proyecto 2/Cranfield/Cranfield/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cranfield
+//
+//  Created by Luis Luna on 9/14/18.
+//  Copyright © 2018 Luis Luna. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
